6307e60419d3504149a573657e4dfdc9837fb39c
Wiebke Koepp <wiebke.koepp@googlemail.com>
2015-09-29 11:37:14 +0200

    Remove removed methods from init as well

--- mlutils/__init__.py
+++ mlutils/__init__.py
@@ -1,10 +1,9 @@
-from complex import clog, cexp, cmul ,cdiv
+from complex import clog, cexp, cmul, cdiv
 from config import load_cfg, base_dir, cfgs_dir
 from gpu import floatx, function, post, gather
 from gridsearch import gridsearch, remove_index_dirs, GridGroup
-from misc import get_key, steps, get_basedir, get_datadir, get_randseed, \
-    sample_list_to_array, get_2d_meshgrid
+from misc import get_key, steps, get_randseed, sample_list_to_array, \
+    get_2d_meshgrid
 from paramhistory import ParameterHistory
 from parameterset import ParameterSet
 from plot import imshow_grid
-
