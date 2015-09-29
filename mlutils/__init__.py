301b46aef771d0ae8a0c62500c6c07f3210f5bac
Wiebke Koepp <wiebke.koepp@googlemail.com>
2015-09-29 11:21:55 +0200

    Add method that returns a 2d meshgrid as either matrix or two vectors
    also remove obsolete methods get data/base_dir

--- mlutils/__init__.py
+++ mlutils/__init__.py
@@ -1,12 +1,10 @@
+from complex import clog, cexp, cmul ,cdiv
 from config import load_cfg, base_dir, cfgs_dir
-from misc import get_key, steps, get_basedir, get_datadir, get_randseed, sample_list_to_array
+from gpu import floatx, function, post, gather
+from gridsearch import gridsearch, remove_index_dirs, GridGroup
+from misc import get_key, steps, get_basedir, get_datadir, get_randseed, \
+    sample_list_to_array, get_2d_meshgrid
 from paramhistory import ParameterHistory
 from parameterset import ParameterSet
-from gpu import floatx, function, post, gather
 from plot import imshow_grid
-from gridsearch import gridsearch, remove_index_dirs, GridGroup
-
-
-
-
 
